89cfc9d438ac0b0a30db1e2ee74fe930e7083dc3
Ali <alikhajeh1@googlemail.com>
2021-03-10 18:39:37 +0000

    Increase AWS IOPs and GCP GPU count

--- terraform-gcp/main.tf
+++ terraform-gcp/main.tf
@@ -20,7 +20,7 @@ resource "google_compute_instance" "instance1" {
 
   guest_accelerator {
     type = "nvidia-tesla-t4" # <<<<< Try changing this to nvidia-tesla-p4 to compare the costs
-    count = 3
+    count = 4
   }
 
   network_interface {
