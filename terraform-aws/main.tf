398e2148b54d6a94da203e673fb154d0e818e64e
Ali <alikhajeh1@googlemail.com>
2021-04-06 14:57:01 +0100

    Test

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -6,21 +6,21 @@ provider "aws" {
   secret_key                  = "mock_secret_key"
 }
 
-resource "aws_instance" "web_app" {
-  ami           = "ami-674cbc1e"
-  instance_type = "m5.4xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
+# resource "aws_instance" "web_app" {
+#   ami           = "ami-674cbc1e"
+#   instance_type = "m5.4xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
 
-  root_block_device {
-    volume_size = 50
-  }
+#   root_block_device {
+#     volume_size = 50
+#   }
 
-  ebs_block_device {
-    device_name = "my_data"
-    volume_type = "io1"                     # <<<<< Try changing this to gp2 to compare costs
-    volume_size = 1000
-    iops        = 500
-  }
-}
+#   ebs_block_device {
+#     device_name = "my_data"
+#     volume_type = "io1"                     # <<<<< Try changing this to gp2 to compare costs
+#     volume_size = 1000
+#     iops        = 700
+#   }
+# }
 
 resource "aws_lambda_function" "hello_world" {
   function_name = "hello_world"
